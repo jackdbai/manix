7f6ad85333b287084bdbc62ecbffbc5bcf4beac2
Jack <jack@jackdbai.dev>
2026-01-20 00:16:08 -0500

    Add apps and aliases

--- home.nix
+++ home.nix
@@ -11,6 +11,14 @@
       eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
     shellAliases = {
+      adblist = "adb shell pm list packages";
+      adblistu = "diff <(adb shell pm list packages) <(adb shell pm list packages -u)";
+      adbreinst = "adb shell cmd package install-existing";
+      adbuninst = "adb shell pm uninstall --user 0";
+      adbkuninst = "adb shell pm uninstall -k --user 0";
+      adbplaydisable = "adb shell pm disable-user --user 0 com.android.vending";
+      adbplayenable = "adb shell pm enable-user --user 0 com.android.vending";
+      cleanup = "sudo nix-collect-garbage -d";
       rebuild = "sudo darwin-rebuild switch --flake ~/Documents/GitHub/manix#main";
     };
   };
